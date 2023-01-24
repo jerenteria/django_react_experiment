ce38911eb5b1da04e4def46b20bdc99c4de6d97b
jerenteria <renteria.juan96@gmail.com>
2023-01-23 19:17:49 -0800

    got serializers to work

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.get_items),
+    path('items/<str:item_id>', views.get_one_item),
 ]
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
 from .models import *
 from .serializers import *
 
-# Create your views here.
 
-
-
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
 def get_items(request):
     all_items = Item.objects.all()
-    serializer = ItemSerializer(items, many=True)
+    # Tell note serializer to pass in all_items and set many to true b/c we are passing many objects
+    serializer = ItemSerializer(all_items, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def get_one_item(request, item_id):
+    one_item = Item.objects.get(id=item_id)
+    # Tell note serializer to pass in all_items and set many to false b/c we are passing only one object
+    serializer = ItemSerializer(one_item, many=False)
     return Response(serializer.data)
